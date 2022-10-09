08c3f455f007069ef01a867af896c7f8f8561497
smahaja <sumit.mahajan@hotmail.com>
2022-10-09 18:17:52 +0530

    Create 19.00.ps1

---
+++ XL/19.00.ps1
@@ -0,0 +1,3 @@
+msiexec /i https://azuredevdisks.blob.core.windows.net/netwoven/XL-Connector-Installer.exe /qn /norestart ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 ENABLE_PSREMOTING=1 REGISTER_MANIFEST=1
+
+
